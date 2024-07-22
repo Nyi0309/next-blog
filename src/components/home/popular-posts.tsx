10392bd156c49e73218f72e1c13b3e86adc0f8b7
WCS-INTERN\paul.koch <paul.koch@pommernprovider.de>
2024-07-22 16:10:11 +0200

    test

--- src/components/home/popular-posts.tsx
+++ src/components/home/popular-posts.tsx
@@ -1,15 +1,29 @@
-import { popularPosts } from "@/lib/placeholder-data";
+"use client";
+
+// import { popularPosts } from "@/lib/placeholder-data";
+import { fetcher, fetchUrl } from "@/lib/utils";
+
+import Link from "next/link";
+import useSWR from "swr";
+import SkeletonCard from "@/components/skeleton/popular_posts_skeleton";
 import { Icons } from "../Icons";
 
-export default function PopularPosts(){
-return (
+export default function PopularPosts() {
+  const { data, error, isLoading } = useSWR(fetchUrl, fetcher);
+
+  if (error) return <div>Failed to load</div>;
+  if (isLoading) return <SkeletonCard />;
+
+  return (
     <ul className="overflow-auto">
-        {popularPosts.map((post) => (
-            <li key={post.title} className="flex items-center gap-2 group cursor-pointer py-2">
-                <Icons.arrowRight className="h-6 w-6 group-hover:translate-x-1 transition-all"/>
-                <p>{post.title}</p>
-            </li>
-        ))}
+      {data?.map((post: { category: string; slug: string; title: string }) => (
+        <Link href={`/blog/${post.category}/${post.slug}`} key={post.title}>
+          <li className="flex items-center gap-2 group cursor-pointer py-2">
+            <Icons.arrowRight className="h-6 w-6 group-hover:translate-x-1 transition-all" />
+            <p>{post.title}</p>
+          </li>
+        </Link>
+      ))}
     </ul>
-);
+  );
 }
