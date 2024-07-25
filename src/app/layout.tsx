d83f886b7d10296015ca7d7789187fcd6dfb37d9
WCS-INTERN\paul.koch <paul.koch@pommernprovider.de>
2024-07-25 11:45:12 +0200

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "@/styles/globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import Footer from "@/components/Footer";
 import { siteConfig } from "@/config/site";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
